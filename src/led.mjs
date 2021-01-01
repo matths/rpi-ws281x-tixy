2e30beb901e86fa51a530f8d5a31649984d48d77
Matthias Dittgen <dittgen@gmail.com>
2021-01-01 11:28:06 +0100

    Hotfix

--- src/led.mjs
+++ src/led.mjs
@@ -1,7 +1,7 @@
 import { performance } from 'perf_hooks';
 import createRenderer from './led/led_renderer.mjs';
 import keyboard from './led/node_keyboard.mjs';
-import tixyLoop from './tixy_loop.mjs';
+import tixyLoop from './tixy/tixy_loop.mjs';
 
 tixyLoop(
   createRenderer,
